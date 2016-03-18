446ff4650b60c268ff7f380f97f20b47591e5db4
tianqing.wang <celeskyking@163.com>
2016-03-18 16:23:08 +0800

    init project

---
+++ src/main/java/com/github/celeskyking/squirrelenium/Driver.java
@@ -0,0 +1,13 @@
+package com.github.celeskyking.squirrelenium;
+
+/**
+ * Created by tianqing.wang
+ * DATE : 16/3/18
+ * TIME : 下午4:19
+ * PROJECT : squirrelenium
+ * PACKAGE : com.github.celeskyking.squirrelenium
+ *
+ * @author <a href="mailto:celeskyking@163.com">tianqing.wang</a>
+ */
+public class Driver {
+}
