cf916b07eb03afec3cad71dafc4c61782ea89594
unknown <>
2016-03-20 09:56:53 +0800

    merge project

---
+++ src/main/java/com/github/celeskyking/squirrelenium/Driver.java
@@ -0,0 +1,20 @@
+package com.github.celeskyking.squirrelenium;
+
+import com.github.celeskyking.squirrelenium.engine.IEngine;
+
+/**
+ * Created by celes on 2016/3/16
+ */
+public class Driver {
+
+    private IEngine engine;
+
+    public Driver(IEngine engine){
+        this.engine = engine;
+    }
+
+    public Driver go(String url){
+        engine.driver().get(url);
+        return this;
+    }
+}
---
+++ src/main/java/com/github/celeskyking/squirrelenium/engine/IEngine.java
@@ -0,0 +1,14 @@
+package com.github.celeskyking.squirrelenium.engine;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Date : 2016/3/17
+ * project : squirrelenium
+ *
+ * @author tianqing.wang.
+ */
+public interface IEngine {
+
+    WebDriver driver();
+}
